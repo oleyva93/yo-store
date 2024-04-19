6fabd72de8d5ac706830351cdf2c5c33a6765d76
Yunior Chavez <yunior.chavez@lighthousetech.io>
2024-04-19 12:59:45 -0600

    add a external subscriber for granular updates

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 
 import createStore from '../../src/index'
 
@@ -25,21 +25,38 @@ const useStore = createStore<StoreData>(
       set((state) => ({ age: state.age + 1 }))
     },
   }),
-  (state) => {
-    console.log('This is a middleware', state)
+  // (state) => {
+  //   console.log('This is a middleware', state)
+  // },
+)
+
+useStore.subscribe(
+  ({ lastName }) => {
+    console.log('the Name is', name)
+  },
+  (lastName) => {
+    console.log('the lastName is', lastName)
   },
 )
 
-const unsubscribe = useStore.subscribe((state) => {
-  if (state.age > 35) {
-    console.log('You are old')
-    return
-  }
-  console.log('you are young')
-})
+useStore.subscribe(
+  ({ name }) => {
+    return name
+  },
+  (name) => {
+    console.log('the Name is', name)
+  },
+)
+
+useStore.subscribe(
+  (state) => state.age,
+  (age) => {
+    console.log('the aaaage ', age)
+  },
+)
 
 function App() {
-  useEffect(() => unsubscribe, [])
+  // useEffect(() => unsubscribe, [])
 
   return (
     <>
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ type SetValueSlice<T> = Partial<T> | ((state: T) => Partial<T>)
 
 type StoreValues<T> = (set: (value: SetValueSlice<T>) => void, get: () => T) => T
 
+let modifyingKey: string = ''
+
 export default function createStore<T>(values: T | StoreValues<T>, middleware?: (state: T) => void) {
   const subscribers = new Set<(data: T) => void>()
 
@@ -11,20 +13,53 @@ export default function createStore<T>(values: T | StoreValues<T>, middleware?: 
     subscribers.add(callback)
     return () => subscribers.delete(callback)
   }
+
+  function externalSubscribe(selector: Function, callback?: Function) {
+    let getKey = ''
+    let handler = {
+      get: function (target: any, prop: string) {
+        getKey = prop
+        return Reflect.get(target, prop)
+      },
+    }
+
+    subscribe((state) => {
+      if (callback) {
+        if (modifyingKey === getKey) {
+          return callback(selector(state) || state[getKey as keyof T])
+        }
+      } else {
+        return selector(state)
+      }
+    })
+    if (callback) {
+      selector(new Proxy(get(), handler))
+    }
+  }
+
   const get: () => T = () => store
 
   const set = (value: SetValueSlice<T>) => {
-    store = {
-      ...store,
-      ...(typeof value === 'function' ? (value as (state: T) => T)(store) : value),
+    const val = typeof value === 'function' ? value(store) : value
+
+    for (const key in val) {
+      store[key] = val[key]
     }
 
     middleware?.(store)
 
-    subscribers.forEach((callback) => callback(store))
+    subscribers.forEach((callback) => {
+      return callback(store)
+    })
   }
 
-  let store = typeof values === 'function' ? (values as StoreValues<T>)(set, get) : values
+  let store = new Proxy(typeof values === 'function' ? (values as StoreValues<T>)(set, get) : values, {
+    set: (target, proName, value) => {
+      modifyingKey = proName as string
+      target[proName] = value
+      return true
+    },
+  })
 
   function useStore<Selector>(selector: (state: T) => Selector): [Selector, typeof set] {
     const handleSelector = useCallback(() => (selector ? selector?.(get()) : get()), [selector])
@@ -34,7 +69,7 @@ export default function createStore<T>(values: T | StoreValues<T>, middleware?: 
     return [state as Selector, set]
   }
 
-  useStore.subscribe = subscribe
+  useStore.subscribe = externalSubscribe
 
   useStore.getState = get
 
